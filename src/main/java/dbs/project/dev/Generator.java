7656553faae12607e13a25bf84d5c8194c51cb77
Nico <cholin@online.de>
2010-07-24 20:04:29 +0200

    refactored

--- src/main/java/dbs/project/dev/Generator.java
+++ src/main/java/dbs/project/dev/Generator.java
@@ -1,16 +1,13 @@
 package dbs.project.dev;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.LinkedList;
-import java.util.List;
+import java.io.*;
+import java.text.*;
+import java.util.*;
 
 import au.com.bytecode.opencsv.CSVReader;
+
 import dbs.project.entity.Player;
+import dbs.project.entity.Team;
 import dbs.project.main.App;
 
 public class Generator {
@@ -22,49 +19,59 @@ public class Generator {
 	 * @retun List<Player>
 	 */
 	public static List<Player> loadSamplePlayersFromCsvFile(String fileName) {
-		List<Player> players = new LinkedList<Player>();
+		List<Player> players = new ArrayList<Player>();
+		List<String[]> csvList = getCsvList(fileName);
 		
-		try {
-			CSVReader reader = getCsvReader(fileName);
-			String[] nextLine;
-			while((nextLine = reader.readNext()) != null) {
-				Date birthday = null;
-				Integer height = 0, weight = 0;
-				try {
-					height = Integer.parseInt(nextLine[3].trim());
-					weight = Integer.parseInt(nextLine[4].trim());
-					birthday = new SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy").parse(nextLine[2].trim());
-				}
-				catch(ParseException e) {}
-				catch(NumberFormatException e) {}
-				
-				Player player = new Player(
-						nextLine[0].trim(),
-						nextLine[1].trim(),
-						"",
-						birthday,
-						"no club",
-						height,
-						weight
-				);
-				
-				players.add(player);
-				
+		for(String[] line : csvList) {
+			Date birthday = null;
+			Integer height = 0, weight = 0;
+			
+			try {
+				height = Integer.parseInt(line[3].trim());
+				weight = Integer.parseInt(line[4].trim());
+				birthday = new SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy").parse(line[2].trim());
 			}
-			reader.close();
-		} catch (FileNotFoundException e) {
-			System.err.println("file not found");
-		} catch (IOException e) {
-			e.printStackTrace();
+			catch(ParseException e) {}
+			catch(NumberFormatException e) {}
+
+			Player player = new Player(
+					line[0].trim(),
+					line[1].trim(),
+					"",
+					birthday,
+					"no club",
+					height,
+					weight
+			);
+			
+			players.add(player);
 		}
 		
 		return players;
 	}
 	
-	private static CSVReader getCsvReader(String fileName) throws FileNotFoundException {
-		String filePath = App.class.getClassLoader().getResource(fileName).getPath()
-        .replaceAll("%20", " ");
-		return new CSVReader(new FileReader(filePath));
+	private static List<String[]> getCsvList(String fileName) {
+		String filePath = App.class.getClassLoader().getResource(fileName).getPath().replaceAll("%20", " ");
+		CSVReader reader;
+		List<String[]> csvList = null;
+		try {
+			reader = new CSVReader(new FileReader(filePath));
+			csvList = reader.readAll();
+			reader.close();
+		} catch (FileNotFoundException e) {
+			System.err.println("file not found");
+		} catch (IOException e) {
+			System.err.println("could not read file");
+		}
+		
+		return csvList;
 	}
+	
+//	public static List<Team> loadSampleTeamsFromCsv(String fileName) {
+//		List<Team> teams = new ArrayList<Team>();
+//		List<String[]> csvList = getCsvList(fileName);
+//		
+//		return teams;
+//	}
 	
 }
