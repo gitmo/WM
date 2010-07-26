7c33a0cc11231fde8f0ba36acd0c26c351a850af
Nico <cholin@online.de>
2010-07-26 18:07:33 +0200

    revived player service

---
+++ src/main/java/dbs/project/service/PlayerService.java
@@ -0,0 +1,13 @@
+
+package dbs.project.service;
+import dbs.project.entity.Match;
+import dbs.project.entity.Player;
+
+public class PlayerService {
+	public static boolean playerHasPlayed(Player player, Match match) {
+	/*TODO: Auswechslungen beachten*/
+		if(match.getGuestLineup().contains(player) || match.getHostLineup().contains(player))
+			return true;
+		return false;
+	}
+}
