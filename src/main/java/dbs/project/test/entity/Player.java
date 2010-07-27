3264814572299e0f5f25a8934ed7916ca943eac5
Nico <cholin@online.de>
2010-07-27 20:16:59 +0200

    dummy test

---
+++ src/main/java/dbs/project/test/entity/Player.java
@@ -0,0 +1,18 @@
+package dbs.project.test.entity;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+
+public class Player {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+}
