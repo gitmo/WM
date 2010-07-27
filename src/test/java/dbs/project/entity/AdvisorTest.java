c7a218fa2dbb8642f646101cc8cd3c96f097212e
Nico <cholin@online.de>
2010-07-27 20:44:52 +0200

    added test structure

---
+++ src/test/java/dbs/project/entity/AdvisorTest.java
@@ -0,0 +1,39 @@
+package dbs.project.entity;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AdvisorTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testAdvisor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdvisorStringStringDateIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTask() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetTask() {
+		fail("Not yet implemented");
+	}
+
+}
