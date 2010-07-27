7363385b3043aa46c43483a0e5017f0059bb0474
Nico <cholin@online.de>
2010-07-27 20:24:45 +0200

    added test suite

---
+++ src/main/java/dbs/project/test/AllTests.java
@@ -0,0 +1,12 @@
+package dbs.project.test;
+
+import org.junit.runner.RunWith;
+import dbs.project.entity.Player;
+import org.junit.runners.*;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	Player.class
+})
+
+public class AllTests {}
