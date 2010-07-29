b993eed57198ed0a8e70d6eee8b25b8e64284da9
sebØrz <sebrz@vegetier.fritz.box>
2010-07-30 00:52:02 +0200

    Refactored PermissionService and new tests

--- src/main/java/dbs/project/service/PermissionService.java
+++ src/main/java/dbs/project/service/PermissionService.java
@@ -3,6 +3,7 @@ package dbs.project.service;
 import static org.junit.Assert.*;
 
 import java.io.Serializable;
+import java.security.acl.Group;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -18,18 +19,31 @@ import dbs.project.util.HibernateUtil;
 public class PermissionService {
 
 	@Test
-	// TODO: Make separte JUnit-Test
+	// TODO: Make separate JUnit-Test
 	public static void main(String[] args) {
-		
-		final String CLASS_NAMES = "dbs.project.entity.Tournament";
+
+		final String plaintextPW = "plaintext";
 
 		Actor a = new Actor();
 		a.setEmail("mudda@fucker.org");
-		a.setPassword_hash("kljsahdgiuy32qo87yfasdjhg532q87rwyu");
+
+		// Encrypt a password and check if the same plain text creates same hash
+		ActorService.setPasswordEncrypted(a, plaintextPW);
+		String rehash = null;
+		try {
+			rehash = PasswordService.getInstance().encrypt(plaintextPW);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		assertEquals(a.getPassword_hash(), rehash);
+
+		Tournament t = new Tournament();
+		t.setYear(1990);
+		t.setName("Turkmenistan");
 
 		Permission p = new Permission();
 		Resource r = new Resource();
-		r.setName(CLASS_NAMES);
+		r.setName(Tournament.class.getName());
 		r.setKey(1990);
 		p.setTypeOfAccess(Permission.AccessType.READ);
 		p.setResource(r);
@@ -37,10 +51,6 @@ public class PermissionService {
 		LinkedList<Permission> permissions = new LinkedList<Permission>();
 		permissions.add(p);
 		a.setPermissions(permissions);
-
-		Tournament t = new Tournament();
-		t.setYear(1990);
-		t.setName("Turkmenistan");
 
 		Session sess = HibernateUtil.getSession();
 		sess.beginTransaction();
@@ -62,29 +72,36 @@ public class PermissionService {
 
 		reqPerm.setTypeOfAccess(Permission.AccessType.UPDATE);
 		assertFalse(isAllowed(a, t, reqPerm));
+
+		reqPerm.setTypeOfAccess(Permission.AccessType.READ);
+		reqPerm.getResource().setKey(1991);
+		assertFalse(isAllowed(a, t, reqPerm));
+
+		reqPerm.getResource().setKey(1990);
+		assertTrue(isAllowed(a, t, reqPerm));
+
+		r.setName(Group.class.getName());
+		assertFalse(isAllowed(a, t, reqPerm));
 	}
 
 	public static <T, S extends Serializable> boolean isAllowed(Actor actor,
 			T reqEntity, Permission reqPerm) {
 
-		System.out.printf("Comparing %s with %s", reqPerm, reqEntity);
+		System.out.printf("Comparing %s with %s\n", reqPerm, reqEntity);
 		List<Permission> perms = actor.getPermissions();
 
 		for (Permission perm : perms) {
 			// TODO: UPDATE -> READ
 			// Check for type of access (READ, UPDATE, ...) first
-			if (reqPerm.getTypeOfAccess() != perm.getTypeOfAccess()) {
-				System.out.println("Diff access type");
+			if (reqPerm.getTypeOfAccess() != perm.getTypeOfAccess())
+				// Different access type -> check next available permission
 				continue;
-			}
 
-			Resource resEntity = perm.getResource();
-			System.out.println("r.getName(): " + resEntity.getName());
-
-			String entityName = reqEntity.getClass().getName();			
-			if (entityName.endsWith(resEntity.getName())) {
+			String entityName = perm.getResource().getName();
+			Class<? extends Object> clazz = reqEntity.getClass();
+			if (entityName.endsWith(clazz.getName())) {
 				Serializable key = reqPerm.getResource().getKey();
-				if (HibernateUtil.getSession().get(resEntity.getName(), key) != null)
+				if (HibernateUtil.getSession().get(clazz, key) != null)
 					return true;
 			}
 		}
