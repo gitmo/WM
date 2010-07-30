30b6404fba8f8b7ca33cadb758669192e40143d3
sebØrz <sebrz@vegetier.fritz.box>
2010-07-30 03:22:32 +0200

    PasswordService uses org.postgresql.util.Base64 now

--- src/main/java/dbs/project/service/PasswordService.java
+++ src/main/java/dbs/project/service/PasswordService.java
@@ -4,9 +4,8 @@ import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-import sun.misc.BASE64Encoder;
+import org.postgresql.util.Base64;
 
-@SuppressWarnings("restriction")
 public final class PasswordService {
 	private static PasswordService instance;
 
@@ -38,7 +37,6 @@ public final class PasswordService {
 		md = MessageDigest.getInstance("SHA");
 		md.update(plaintext.getBytes("UTF-8"));
 		byte raw[] = md.digest();
-		String hash = (new BASE64Encoder()).encode(raw);
-		return hash; // step 6
+		return Base64.encodeBytes(raw);
 	}
 }
