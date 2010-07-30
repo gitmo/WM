fb72fbbc988997daee8b1a3e67daf19736b5b14b
sebØrz <sebrz@vegetier.fritz.box>
2010-07-30 05:35:23 +0200

    Use getResource in PlayerCsvFileGenerator

--- src/main/java/dbs/project/generator/PlayerCsvFileGenerator.java
+++ src/main/java/dbs/project/generator/PlayerCsvFileGenerator.java
@@ -2,8 +2,15 @@ package dbs.project.generator;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.Writer;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
@@ -12,37 +19,37 @@ import java.util.Vector;
 
 public class PlayerCsvFileGenerator {
 
-	private static final String INPUT_FOLDER = "dev/generator/";
-	private static final String OUTPUT_FOLDER = "dev/";
-	private static final String FIRSTNAME_FILE = TournamentGenerator
-			.getAbsoluteFilePath(INPUT_FOLDER + "spielerVor");
-	private static final String LASTNAME_FILE = TournamentGenerator
-			.getAbsoluteFilePath(INPUT_FOLDER + "spielerNach");
+	private static final String OUTPUT_FOLDER = "/dev/";
+	private static final String OUTPUT_FILE = "players.csv";
+	private static final String FIRSTNAME_FILE = "/dev/generator/spielerVor";
+	private static final String LASTNAME_FILE = "/dev/generator/spielerNach";
 	private static final int AMOUNT_OF_PLAYERS = 5000;
 
 	private static final int MIN_HEIGHT = 155, MAX_HEIGHT = 205,
 			MIN_WEIGHT = 55, MAX_WEIGHT = 95;
 
-	private static final String CSV_OUTPUT_FILE = TournamentGenerator
-			.getAbsoluteFilePath(OUTPUT_FOLDER + "players.csv");
-
-	/*
-	 * 
-	 * Erzeugt die Datei player_dev.csv Vorname, Nachname, Geburtszeitpunkt,
+	/**
+	 * Erzeugt die Datei player.csv Vorname, Nachname, Geburtszeitpunkt,
 	 * Groesse, Gewicht
+	 * 
+	 * @throws Exception
 	 */
 	public static void generatePlayers() throws Exception {
 		Vector<String> firstnames = new Vector<String>();
 		Vector<String> lastnames = new Vector<String>();
 
-		BufferedReader in = new BufferedReader(new FileReader(FIRSTNAME_FILE));
+		BufferedReader in = new BufferedReader(new InputStreamReader(
+				ClassLoader.class.getResourceAsStream(FIRSTNAME_FILE)));
+
 		String line;
 		while ((line = in.readLine()) != null) {
 			firstnames.add(line.trim());
 		}
 		in.close();
 
-		in = new BufferedReader(new FileReader(LASTNAME_FILE));
+		in = new BufferedReader(new InputStreamReader(
+				ClassLoader.class.getResourceAsStream(LASTNAME_FILE)));
+
 		while ((line = in.readLine()) != null) {
 			lastnames.add(line.trim());
 		}
@@ -55,7 +62,9 @@ public class PlayerCsvFileGenerator {
 		Calendar birthday = new GregorianCalendar();
 		SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
 
-		BufferedWriter bw = new BufferedWriter(new FileWriter(CSV_OUTPUT_FILE));
+		String path = ClassLoader.class.getResource(OUTPUT_FOLDER).getPath()
+				+ "/" + OUTPUT_FILE;
+		BufferedWriter bw = new BufferedWriter(new FileWriter(path));
 		for (int i = 0; i < AMOUNT_OF_PLAYERS; i++) {
 			lastname = lastnames.get(randomizer.nextInt(lastnames.size()));
 			firstname = firstnames.get(randomizer.nextInt(firstnames.size()));
