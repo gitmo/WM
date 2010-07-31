8a791ec55757658b007563fc65a883268664faaa
sebØrz <sebrz@vegetier.fritz.box>
2010-07-31 16:44:41 +0200

    docs

--- makerelease.sh
+++ makerelease.sh
@@ -33,7 +33,7 @@ mvn -DskipTests=true assembly:assembly
 cp -p target/WM-0.0.1-SNAPSHOT-jar-with-dependencies.jar release/
 
 # Sources
-tar -vcf release/sources.tgz src/ pom.xml runtests.sh
+tar -vcf release/sources.tar.gz src/ pom.xml runtests.sh
 
 mkdir release/doc
 cp -rp doc/tex/*pdf release/doc
